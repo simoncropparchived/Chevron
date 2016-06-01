b19432e92794e878da5c100d8f3c3f72209beeae
Simon Cropp <simon.cropp@gmail.com>
2016-06-01 22:36:47 +1000

    bump v8

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Chevron")]
 [assembly: AssemblyProduct("Chevron")]
-[assembly: AssemblyVersion("1.5.2")]
-[assembly: AssemblyFileVersion("1.5.2")]
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0")]
