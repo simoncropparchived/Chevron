311bd469c4fde92bf47c4da13751155b22d67a0c
Simon Cropp <simon.cropp@gmail.com>
2017-12-09 09:44:24 +1100

    bump version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Chevron")]
 [assembly: AssemblyProduct("Chevron")]
-[assembly: AssemblyVersion("1.8.0")]
-[assembly: AssemblyFileVersion("1.8.0")]
+[assembly: AssemblyVersion("1.9.0")]
+[assembly: AssemblyFileVersion("1.9.0")]
